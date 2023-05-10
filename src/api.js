17030e3836cec2ae86d298b5e3ef7beafc500d04
Oseas Oliveira <126169147+OseOliv@users.noreply.github.com>
2023-05-09 22:22:45 -0300

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "a9041e2783mshadf4a115cae1dc6p10f006jsn24f7a6ee5ef0",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "ddacb978c15c366634cd809e6d77be93";
